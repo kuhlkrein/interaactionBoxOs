12ae19f7f87a189010fdbc38430f994a4f15b85d
Riou Sebastien <sebastien.riou@univ-grenoble-alpes.fr>
2020-12-11 11:33:31 +0100

    Update interaactionBoxOS-linux.sh

--- src/main/bin/interaactionBoxOS-linux.sh
+++ src/main/bin/interaactionBoxOS-linux.sh
@@ -1,36 +1,64 @@
-#!/bin/sh
-
-set -e
-
-MAIN_JAR_FILE=interaactionBoxOs.jar
-
-WORKING_DIR=$(pwd)
-
-echo "WORKING_DIR = ${WORKING_DIR}"
-
-SCRIPT_DIR=$(dirname $0)
-
-echo "SCRIPT_DIR = ${SCRIPT_DIR}"
-
-LIB_DIR=${SCRIPT_DIR}/../lib
-
-echo "LIB_DIR = ${LIB_DIR}"
-
-LIB_DIR_RELATIVE=$(realpath --relative-to="${WORKING_DIR}" "${LIB_DIR}")
-
-echo "LIB_DIR_RELATIVE = ${LIB_DIR_RELATIVE}"
-
-CLASSPATH=$(find ./$LIB_DIR_RELATIVE -name "*.jar" | sort | tr '\n' ':')
-
-export JAVA_HOME=${LIB_DIR}/jre
-
-echo "JAVA_HOME = ${JAVA_HOME}"
-
-export PATH=${JAVA_HOME}/bin:${PATH}
-
+#!/bin/sh  
+  
+set -e  
+  
+MAIN_JAR_FILE=interaactionBoxOs.jar  
+  
+WORKING_DIR=$(pwd)  
+  
+echo "WORKING_DIR = ${WORKING_DIR}"  
+  
+SCRIPT_DIR=$(dirname $0)  
+  
+echo "SCRIPT_DIR = ${SCRIPT_DIR}"  
+  
+LIB_DIR=${SCRIPT_DIR}/../lib  
+  
+echo "LIB_DIR = ${LIB_DIR}"  
+  
+LIB_DIR_RELATIVE=$(realpath --relative-to="${WORKING_DIR}" "${LIB_DIR}")  
+  
+echo "LIB_DIR_RELATIVE = ${LIB_DIR_RELATIVE}"  
+  
+CLASSPATH=$(find ./$LIB_DIR_RELATIVE -name "*.jar" | sort | tr '\n' ':')  
+  
+export JAVA_HOME=${LIB_DIR}/jre  
+  
+echo "JAVA_HOME = ${JAVA_HOME}"  
+  
+export PATH=${JAVA_HOME}/bin:${PATH}  
+  
 echo "PATH = ${PATH}"
 
-export JAVA_CMD="java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main"
+FILE=${SCRIPT_DIR}/configuration.conf
+if [ -f "$FILE" ]; then
+    echo "exists"
+    gazePlaySave=$(head -n 1 "${FILE}")
+    if [ -d "$gazePlaySave" ]; then
+      
+      export JAVA_CMD="${WORKING_DIR}/../lib/jre/bin/java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main $gazePlaySave"
+
+      echo "Executing command line: $JAVA_CMD"
+
+      ${JAVA_CMD}
+      
+      exit
+    fi
+fi
+
+gazePlaySave=""
+
+while [ ! -d "$gazePlaySave" ] 
+do
+read -p "Entrez le chemin menant au dossier d'installation de GazePlay: " gazePlaySave
+if [ ! -d "$gazePlaySave" ]; then
+ echo "Erreur: \"$gazePlaySave\" n'est pas un dossier valide."
+fi
+done
+
+echo "$gazePlaySave" > configuration.conf
+  
+export JAVA_CMD="${WORKING_DIR}/../lib/jre/bin/java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main $gazePlaySave"
 
 echo "Executing command line: $JAVA_CMD"
 
