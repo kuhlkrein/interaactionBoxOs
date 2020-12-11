fba2eea035173e26e5e25ac092ef8188667de87d
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2020-12-11 10:18:45 +0100

    launch java depending on the OS

--- src/main/java/main/process/GazePlayProcess.java
+++ src/main/java/main/process/GazePlayProcess.java
@@ -5,6 +5,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import main.ProgressButton;
 import main.SecondStage;
+import main.UtilsOS;
 import main.gaze.devicemanager.AbstractGazeDeviceManager;
 
 import java.io.File;
@@ -37,7 +38,12 @@ public class GazePlayProcess implements AppProcess {
     }
 
     private ProcessBuilder createGazePlayBuilder() {
-        String javaBin = gazePlayInstallationRepo + "\\lib\\jre\\bin\\java.exe";
+        String javaBin;
+        if(UtilsOS.isWindows()) {
+            javaBin = gazePlayInstallationRepo + "\\lib\\jre\\bin\\java.exe";
+        } else {
+            javaBin = gazePlayInstallationRepo + "\\lib\\jre\\bin\\java";
+        }
         String classpath = gazePlayInstallationRepo + "\\lib\\*";
 
         LinkedList<String> commands = new LinkedList<>(Arrays.asList(javaBin, "-cp", classpath, "net.gazeplay.GazePlayLauncher"));
